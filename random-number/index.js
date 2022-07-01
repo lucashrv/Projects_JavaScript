c048e4f0ba4f8f54a0ff15eced466d51e784d55c
lucashrv <lucas.hrv@outlook.com>
2022-07-01 14:50:47 -0300

    V 1.0

--- random-number/index.js
+++ random-number/index.js
@@ -1,10 +1,15 @@
 function randomNum(){
-    let init = document.querySelector('#init').value
-    let final = document.querySelector('#final').value
-    init = ~~init
-    final = ~~final
+    const init = parseInt(document.querySelector('#init').value)
+    const final = parseInt(document.querySelector('#final').value)
+    const result = document.querySelector('#result')
 
     const random = Math.floor(Math.random() * (final - init + 1) + init)
 
-    document.querySelector('#result').value = random
+    if(isNaN(init) || isNaN(final)){
+        result.className = 'form-control text-danger'
+        return result.value = 'Invalid Value'
+    }
+
+    result.className = 'form-control text-success'
+    result.value = random
 }
